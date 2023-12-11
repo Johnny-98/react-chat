139b9031b8309b9b646bcbb16cbf4d986c58bbd5
Peter <petiodjonov@yahoo.com>
2023-12-11 18:44:03 +0200

    Cleanup login logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,27 +7,35 @@ import Chat from './Chat';
 const App: React.FC = () => {
   const socket: Socket = io.connect("http://localhost:3001")
   const[username, setUsername] = useState("");
+  const[showChat, setShowChat] = useState(false);
 
   const login = () => {
     if(username !== ""){
       socket.emit("log_in", username)
+      setShowChat(true);
     }
   };
 
   return (
     <div className="App">
-
-        <div>
-          {/* accessing value of input */}
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Join chat</h3>
           <input 
             type="text"  
             placeholder='type username' 
+            //accessing value of input
             onChange={(event) => { setUsername(event.target.value)}}/>
           <button onClick={login} >Login</button>
         </div>
-
-        <Chat socket={socket} username={username} />
-
+        ):(
+          <div>
+            <p>Hello {username}</p>
+            <button>Log  out</button>
+            <p />
+            <Chat socket={socket} username={username} />
+          </div> 
+      )}
     </div>
   );
 }
